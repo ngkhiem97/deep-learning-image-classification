df393c84becd537c58489f87af5c6ff1779b14f5
Khiem Nguyen <ng.khiem97vn@gmail.com>
2022-09-02 18:28:19 -0400

    refactor for CNN Model 2

--- lenet_dropout.py
+++ lenet_dropout.py
@@ -41,21 +41,23 @@ tanhLayer2 = layers.TanhLayer()
 poolingLayer2 = layers.PoolingLayer(2, 2)
 flattenLayer = layers.FlattenLayer()
 fcLayer3 = layers.FullyConnectedLayer(2400, 120, xavier_init = True)
+dropoutLayer3 = layers.DropoutLayer(0.9)
 tanhLayer3 = layers.TanhLayer()
 fcLayer4 = layers.FullyConnectedLayer(120, 84, xavier_init = True)
+dropoutLayer4 = layers.DropoutLayer(0.8)
 tanhLayer4 = layers.TanhLayer()
 fcLayer5 = layers.FullyConnectedLayer(84, 10, xavier_init = True)
 softmaxLayer = layers.SoftmaxLayer()
 crossEntropyLoss = layers.CrossEntropy()
 lenet = [convLayer1, tanhLayer1, poolingLayer1,
          convLayer2, tanhLayer2, poolingLayer2, flattenLayer, 
-         fcLayer3, tanhLayer3, 
-         fcLayer4, tanhLayer4, 
+         fcLayer3, dropoutLayer3, tanhLayer3, 
+         fcLayer4, dropoutLayer4, tanhLayer4, 
          fcLayer5, softmaxLayer, crossEntropyLoss]
 
-util.train_model(lenet, X_train, Y_train_encoded, X_test, Y_test_encoded, "lenet", 
+util.train_model(lenet, X_train[:1000], Y_train_encoded[:1000], X_test[:1000], Y_test_encoded[:1000], "lenet", 
                  learning_rate = 0.0001, 
-                 max_epochs = 20, 
+                 max_epochs = 50, 
                  batch_size = 1,
                  condition = 10e-10,
                  skip_first_layer=False)
